8868cf96940ce23a9d83a09798cdfbb02dea7845
Gede Bagus <bdarmagita@gmail.com>
2021-05-08 19:28:01 +0700

    Make player can challage level 1/2/3 from main hall

---
+++ Assets/MainHall/Level3Scene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level3Scene : MonoBehaviour
+{
+    //public static bool GameIsPaused = false;
+    public GameObject Level1;
+    private int SceneLoad;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Level1.SetActive(true);
+            Time.timeScale = 0f;
+            //GameIsPaused = true;
+        }
+    }
+
+    public void No()
+    {
+        Level1.SetActive(false);
+        Time.timeScale = 1f;
+        //GameIsPaused = false;
+    }
+
+    public void Yes()
+    {
+        Time.timeScale = 1f;
+        SceneLoad = SceneManager.GetActiveScene().buildIndex + 3;
+        SceneManager.LoadScene(SceneLoad);
+    }
+}
